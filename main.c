3b0d7cc79682f4b4e033172503ac0e579d612cb4
Chatchai J <cj@coe.psu.ac.th>
2014-10-27 12:45:44 +0000

    Change in tables
    	- printing
    	- initialization
    	- also add simple testing

--- main.c
+++ main.c
@@ -2,14 +2,39 @@
 
 char	table[3][3]={ {0,0},{0,0},{0,0} };
 
+int	init_table() {
+	int i,j;
+
+	for (i=0;i<3;i++)
+		for (j=0;j<3;j++)
+			table[i][j] = 0;
+		
+	return 0;
+
+}
+
 int	print_table() {
-	printf(	"...\n"
-		"...\n"
-		"...\n");
+	int	i,j;
+
+	for (i=0;i<3;i++) {
+		for (j=0;j<3;j++) {
+			int c = table[i][j];
+			printf("%c", c==0?'.':c);
+		}
+		printf("\n");
+	}
+	printf("\n");
+
 	return 0;
 }
 
 int	main(void) {
+	print_table();
+	table[0][0] = 'x';
+	print_table();
+	table[1][1] = 'o';
+	print_table();
+	table[2][2] = 'z';
 	print_table();
 	return 0;
 }
